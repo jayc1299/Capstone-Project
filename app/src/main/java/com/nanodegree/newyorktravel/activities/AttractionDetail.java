8608ed8fa1978a12803eb5fefd23eced179727a1
Jay Cunnington <jay.cunnington@ctt.co.uk>
2018-09-06 15:35:58 +0100

    Adding review score to the attraction detai page.

--- app/src/main/java/com/nanodegree/newyorktravel/activities/AttractionDetail.java
+++ app/src/main/java/com/nanodegree/newyorktravel/activities/AttractionDetail.java
@@ -3,6 +3,7 @@ package com.nanodegree.newyorktravel.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,17 +12,30 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.nanodegree.newyorktravel.R;
 import com.nanodegree.newyorktravel.holders.Attraction;
+import com.nanodegree.newyorktravel.holders.Review;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AttractionDetail extends AppCompatActivity {
 
     public static final int ACTIVITY_DETAIL_REQ_CODE = 1;
     public static final String TAG_ATTRACTION = "tag_attraction";
+    private static final String TAG = AttractionDetail.class.getSimpleName();
 
     private Attraction attraction;
 
@@ -40,6 +54,7 @@ public class AttractionDetail extends AppCompatActivity {
 
             attraction = bundle.getParcelable(TAG_ATTRACTION);
             setupAttraction(attraction);
+            getReviews();
         }
 
         //Map button
@@ -114,5 +129,31 @@ public class AttractionDetail extends AppCompatActivity {
         CollapsingToolbarLayout collapsingToolbarLayout =  findViewById(R.id.collapsing_toolbar);
         collapsingToolbarLayout.setTitle(title);
         collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+    }
+
+    private void getReviews() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reviewRef = database.getReference("reviews");
+        Query reviewQuery = reviewRef.orderByChild("attractionId").equalTo(attraction.getId());
+
+        reviewQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                float totalScore = 0;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Review review = snapshot.getValue(Review.class);
+                    if (review != null) {
+                        totalScore += review.getReviewScore();
+                    }
+                }
+
+                ((RatingBar) findViewById(R.id.activity_detail_star_rating)).setRating(totalScore);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e(TAG, "onCancelled: ", databaseError.toException());
+            }
+        });
     }
 }
